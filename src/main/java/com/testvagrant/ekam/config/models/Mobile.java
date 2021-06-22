af6787776c58ee71c1884c3527bb95a596bf503c
krishnb <kbandame@gmail.com>
2021-06-22 14:06:38 +0530

    Added configs for Mobile: ServerArgs | DeviceFilters

--- src/main/java/com/testvagrant/ekam/config/models/Mobile.java
+++ src/main/java/com/testvagrant/ekam/config/models/Mobile.java
@@ -11,6 +11,9 @@ public class Mobile {
     private String feed = "";
     private String target = "android";
     private String hub = "";
+    private String deviceFilters = "";
+    private String serverArgs = "";
+    private String executables = "";
 
 
     @Inject(optional = true)
@@ -26,6 +29,41 @@ public class Mobile {
     @Inject(optional = true)
     public void setHub(@Named("mobile.hub") String hub) {
         this.hub = EkamPropertyReader.updateProperty("mobile.hub", hub);
+    }
+
+    @Inject(optional = true)
+    public void setDeviceFilters(@Named("mobile.filters") String deviceFilters) {
+        this.deviceFilters = EkamPropertyReader.updateProperty("mobile.filters", deviceFilters);
+    }
+
+    @Inject(optional = true)
+    public void setServerArgs(@Named("mobile.server.args") String serverArgs) {
+        this.serverArgs = EkamPropertyReader.updateProperty("mobile.server.args", deviceFilters);;
+    }
+
+    @Inject(optional = true)
+    public void setExecutables(@Named("mobile.executables") String executables) {
+        this.executables = EkamPropertyReader.updateProperty("mobile.executables", deviceFilters);;
+    }
+
+    public boolean isAny() {
+        return this.target.equalsIgnoreCase("any");
+    }
+
+    public boolean isRemote() {
+        return !this.hub.isEmpty();
+    }
+
+    public boolean isServerArgsProvided() {
+        return !this.serverArgs.isEmpty();
+    }
+
+    public boolean isDeviceFiltersProvided() {
+        return !this.deviceFilters.isEmpty();
+    }
+
+    public boolean isExecutablesProvided() {
+        return !this.executables.isEmpty();
     }
 
     @Override
