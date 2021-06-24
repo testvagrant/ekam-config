90b60a32d68fe6d7233eb9cd6ca0c79b71127866
krishnb <kbandame@gmail.com>
2021-06-24 12:14:42 +0530

    Updated config tests

--- src/test/java/com/testvagrant/ekam/config/EkamConfigTest.java
+++ src/test/java/com/testvagrant/ekam/config/EkamConfigTest.java
@@ -1,53 +1,37 @@
 package com.testvagrant.ekam.config;
 
-import com.google.inject.Inject;
 import com.testvagrant.ekam.config.models.Ekam;
 import org.testng.Assert;
-import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
-@Guice(modules = EkamConfigModule.class)
-public class EkamConfigTest {
 
-  @Inject Ekam ekam;
+public class EkamConfigTest {
 
   @Test
   public void whenConfigIsNotSetEkamShouldReadFromDefaultDotPropertiesFile() {
+    Ekam ekam =
+            com.google.inject.Guice.createInjector(new EkamConfigModule()).getInstance(Ekam.class);
     Assert.assertEquals(ekam.getEnv(), "qa");
-    Assert.assertEquals(ekam.getMobile().getHub(), "browserstack");
+    Assert.assertEquals(ekam.getMobile().getHub(), "");
   }
 
   @Test
   public void whenConfigIsSetEkamShouldReadFromConfigDotPropertiesFile() {
     System.setProperty("config", "remote");
+    System.setProperty("env","");
+    System.setProperty("mobile.feed","");
     Ekam ekam =
-        com.google.inject.Guice.createInjector(new EkamConfigModule()).getInstance(Ekam.class);
+            com.google.inject.Guice.createInjector(new EkamConfigModule()).getInstance(Ekam.class);
     Assert.assertEquals(ekam.getEnv(), "staging");
     Assert.assertEquals(ekam.getMobile().getFeed(), "remoteTestFeed");
-  }
-
-  @Test
-  public void whenConfigIsSetEkamShouldAutoFillDefaultProperties() {
-    Assert.assertEquals(ekam.getMobile().getFeed(), "mobileTestFeed");
-    Assert.assertEquals(ekam.getWeb().getFeed(), "webTestFeed");
-    System.setProperty("config", "remote");
-    Ekam ekam =
-        com.google.inject.Guice.createInjector(new EkamConfigModule()).getInstance(Ekam.class);
-    Assert.assertEquals(ekam.getMobile().getFeed(), "remoteTestFeed");
-    Assert.assertEquals(ekam.getWeb().getFeed(), "webTestFeed");
   }
 
   @Test
   public void whenConfigValueIsNotDefinedInPropertiesFileDefaultValueShouldBeLoaded() {
     System.setProperty("config", "remote");
     Ekam ekam =
-        com.google.inject.Guice.createInjector(new EkamConfigModule()).getInstance(Ekam.class);
+            com.google.inject.Guice.createInjector(new EkamConfigModule()).getInstance(Ekam.class);
     Assert.assertTrue(ekam.getIntegrations().getSlack().isNotify());
     Assert.assertFalse(ekam.getIntegrations().getSlack().isNotifyMeEveryTime());
-  }
-
-  @Test
-  public void namedTest() {
-    System.out.println(ekam);
   }
 }
