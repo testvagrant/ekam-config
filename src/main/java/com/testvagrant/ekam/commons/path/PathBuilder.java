02b4dd745c402922240fba3024b3ce0e47002958
Sudarshan <sudarshan@MacBook-Pro.local>
2021-07-26 15:40:32 +0530

    Updated PathBuilder

--- src/main/java/com/testvagrant/ekam/commons/path/PathBuilder.java
+++ src/main/java/com/testvagrant/ekam/commons/path/PathBuilder.java
@@ -5,55 +5,55 @@ package com.testvagrant.ekam.commons.path;
  */
 public final class PathBuilder {
 
-  private final StringBuilder sb = new StringBuilder();
+    private final StringBuilder sb = new StringBuilder();
 
-  /**
-   * Creates a new {@code PathBuilder} instance
-   *
-   * @param path the initial path
-   */
-  public PathBuilder(final String path) {
+    /**
+     * Creates a new {@code PathBuilder} instance
+     *
+     * @param path the initial path
+     */
+    public PathBuilder(final String path) {
 
-    if (path == null || path.isEmpty() || path.charAt(0) != '/') {
-      throw new IllegalArgumentException("Path '" + path + "' is not absolute");
+        if (path == null || path.isEmpty()) {
+            throw new IllegalArgumentException("Path '" + path + "' is not absolute");
+        }
+
+        sb.append(path);
     }
 
-    sb.append(path);
-  }
+    /**
+     * Appends a new path fragment
+     *
+     * @param path the path fragment to append
+     * @return this instance
+     */
+    public PathBuilder append(final String path) {
 
-  /**
-   * Appends a new path fragment
-   *
-   * @param path the path fragment to append
-   * @return this instance
-   */
-  public PathBuilder append(final String path) {
+        if (path == null || path.isEmpty()) {
+            throw new IllegalArgumentException("Path '" + path + "' is null or empty");
+        }
 
-    if (path == null || path.isEmpty()) {
-      throw new IllegalArgumentException("Path '" + path + "' is null or empty");
+        boolean trailingSlash = sb.charAt(sb.length() - 1) == '/';
+        boolean leadingSlash = path.charAt(0) == '/';
+
+        if (trailingSlash && leadingSlash) {
+            sb.append(path.substring(1));
+        } else if (!trailingSlash && !leadingSlash) {
+            sb.append('/').append(path);
+        } else {
+            sb.append(path);
+        }
+
+        return this;
     }
 
-    boolean trailingSlash = sb.charAt(sb.length() - 1) == '/';
-    boolean leadingSlash = path.charAt(0) == '/';
-
-    if (trailingSlash && leadingSlash) {
-      sb.append(path.substring(1));
-    } else if (!trailingSlash && !leadingSlash) {
-      sb.append('/').append(path);
-    } else {
-      sb.append(path);
+    /**
+     * Returns the path
+     *
+     * @return the path
+     */
+    @Override
+    public String toString() {
+        return sb.toString();
     }
-
-    return this;
-  }
-
-  /**
-   * Returns the path
-   *
-   * @return the path
-   */
-  @Override
-  public String toString() {
-    return sb.toString();
-  }
 }
