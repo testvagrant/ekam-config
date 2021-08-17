42a0d46e423326459fd86de9391e1f358abca26e
Sudarshan <sudarshan@MacBook-Pro.local>
2021-08-17 15:55:47 +0530

    Remove EkamConfigModule

--- src/test/java/com/testvagrant/ekam/config/EkamConfigTest.java
+++ src/test/java/com/testvagrant/ekam/config/EkamConfigTest.java
@@ -1,19 +1,20 @@
 package com.testvagrant.ekam.config;
 
 import com.testvagrant.ekam.config.models.EkamConfig;
+import com.testvagrant.ekam.config.properties.ConfigPropertyLoader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.ClearSystemProperty;
 import org.junitpioneer.jupiter.SetSystemProperty;
+
+import java.util.Properties;
 
 public class EkamConfigTest {
 
   @Test
   @ClearSystemProperty(key = "config")
   public void whenConfigIsNotSetEkamShouldReadFromDefaultDotPropertiesFile() {
-    EkamConfig ekamConfig =
-        com.google.inject.Guice.createInjector(new EkamConfigModule())
-            .getInstance(EkamConfig.class);
+    EkamConfig ekamConfig = ekamConfig();
     Assertions.assertEquals(ekamConfig.getEnvironments().getEnv(), "qa");
     Assertions.assertEquals(ekamConfig.getMobile().getHub(), "");
   }
@@ -23,9 +24,7 @@ public class EkamConfigTest {
   @SetSystemProperty(key = "env", value = "")
   @SetSystemProperty(key = "mobile.feed", value = "")
   public void whenConfigIsSetEkamShouldReadFromConfigDotPropertiesFile() {
-    EkamConfig ekamConfig =
-        com.google.inject.Guice.createInjector(new EkamConfigModule())
-            .getInstance(EkamConfig.class);
+    EkamConfig ekamConfig = ekamConfig();
     Assertions.assertEquals(ekamConfig.getEnvironments().getEnv(), "staging");
     Assertions.assertEquals(ekamConfig.getMobile().getFeed(), "remoteTestFeed");
   }
@@ -35,9 +34,7 @@ public class EkamConfigTest {
   @SetSystemProperty(key = "env", value = "")
   @SetSystemProperty(key = "mobile.feed", value = "abracadabra")
   public void whenConfigIsSetAndAPropertyIsOverRiddenEkamShouldReadTheOverRiddenProperty() {
-    EkamConfig ekamConfig =
-        com.google.inject.Guice.createInjector(new EkamConfigModule())
-            .getInstance(EkamConfig.class);
+    EkamConfig ekamConfig = ekamConfig();
     Assertions.assertEquals(ekamConfig.getEnvironments().getEnv(), "staging");
     Assertions.assertEquals(ekamConfig.getMobile().getFeed(), "abracadabra");
   }
@@ -45,9 +42,7 @@ public class EkamConfigTest {
   @Test
   @SetSystemProperty(key = "config", value = "remote")
   public void whenConfigValueIsNotDefinedInPropertiesFileDefaultValueShouldBeLoaded() {
-    EkamConfig ekamConfig =
-        com.google.inject.Guice.createInjector(new EkamConfigModule())
-            .getInstance(EkamConfig.class);
+    EkamConfig ekamConfig = ekamConfig();
     Assertions.assertTrue(ekamConfig.getIntegrations().getSlack().isNotify());
     Assertions.assertFalse(ekamConfig.getIntegrations().getSlack().isNotifyMeEveryTime());
   }
@@ -55,9 +50,7 @@ public class EkamConfigTest {
   @Test
   @SetSystemProperty(key = "env", value = "")
   public void whenSpecificEnvsAreNotProvidedDefaultEnvShouldBeUsed() {
-    EkamConfig ekamConfig =
-        com.google.inject.Guice.createInjector(new EkamConfigModule())
-            .getInstance(EkamConfig.class);
+    EkamConfig ekamConfig = ekamConfig();
     Assertions.assertEquals(ekamConfig.getEnvironments().getApiEnv(), "qa");
     Assertions.assertEquals(ekamConfig.getEnvironments().getDbEnv(), "qa");
     Assertions.assertEquals(ekamConfig.getEnvironments().getWebEnv(), "qa");
@@ -70,12 +63,15 @@ public class EkamConfigTest {
   @SetSystemProperty(key = "env", value = "")
   @SetSystemProperty(key = "config", value = "env")
   public void whenSpecificEnvsSetProvidedDefaultEnvShouldNotBeUsed() {
-    EkamConfig ekamConfig =
-        com.google.inject.Guice.createInjector(new EkamConfigModule())
-            .getInstance(EkamConfig.class);
+    EkamConfig ekamConfig = ekamConfig();
     Assertions.assertEquals(ekamConfig.getEnvironments().getApiEnv(), "staging");
     Assertions.assertEquals(ekamConfig.getEnvironments().getMobileEnv(), "qa");
     Assertions.assertEquals(ekamConfig.getEnvironments().getWebEnv(), "qa");
     Assertions.assertEquals(ekamConfig.getEnvironments().getDbEnv(), "dbstaging");
+  }
+
+  private EkamConfig ekamConfig() {
+    Properties properties = new ConfigPropertyLoader().loadConfig();
+    return new EkamConfig(properties);
   }
 }
